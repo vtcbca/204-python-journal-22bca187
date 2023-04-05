3191ad9e2c33e9bba43f4d1aaf265767625326cc
22bca187 <127925751+22bca187@users.noreply.github.com>
2023-04-05 20:45:53 +0530

    Delete numpysolution directory

--- numpysolution/script12.py
+++
@@ -1,6 +0,0 @@
-#scrip12....Write a NumPy program to convert a list of numeric value into a one-dimensional NumPy array. Print original list and array with its dimension , size and byte occupied in memory
-import numpy as np
-l1 = [50, 100, 200, 332]
-print("Original List:",l1)
-v = np.array(l1)
-print("One-dimensional NumPy array: ",v)
--- numpysolution/script14.py
+++
@@ -1,6 +0,0 @@
-#Write a NumPy program to get the values and indices of the elements that are bigger than 10 in a given array.
-import numpy as np
-a = np.array([[0, 10, 20], [30, 40, 50]])
-print("Original array: ")
-print(a)
-print("Values is bigger than 10 =", a[a>10])
