2422bd6d5d282e0bdc04e31c635d80e3e99130cc
22bca187 <127925751+22bca187@users.noreply.github.com>
2023-04-05 12:02:18 +0530

    Delete stringsolution directory

--- stringsolution/scrip1.py
+++
@@ -1,7 +0,0 @@
-#python script to check string is palindrome or not
-x=input("Enter any String :")
-y=x[::-1]
-if(x==y):
-    print("String is Palindrome.")
-else:
-    print("String is Not Palindrome.")
